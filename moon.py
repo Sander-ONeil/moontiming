27d0ce088159125583a8e401e802edc4283f6dff
Sander O'Neil <58371105+Sander-ONeil@users.noreply.github.com>
2024-09-16 17:40:45 -0400

    Update moon.py

--- moon.py
+++ moon.py
@@ -78,6 +78,11 @@ class Orbit:
         # print(message)
         return day/self.period*2*math.pi +self.day_to_angle_adjustment
 
+    def angle_to_day(self,angle):
+        # print('dayanglefunc',self.day_to_angle_adjustment)
+        # print(message)
+        return (angle)/(2*math.pi)*self.period
+
     def goal_angle_to_orbital_pos(self, goal_angle):
         angle = goal_angle + 0
         M = goal_angle - self.e * np.sin(goal_angle - math.pi)
@@ -98,13 +103,55 @@ class Orbit:
     def goal_angle_to_orbital_pos_withinclination(self, goal_angle):
         p = self.goal_angle_to_orbital_pos( goal_angle)
 
-        inclination_matrix = rotationMatrix_single(1,np.deg2rad( inclination))
+        # inclination_matrix = rotationMatrix(1,np.deg2rad(self.inclination)+goal_angle*0)
 
-        inclination_to_ecliptic = self.inclination_to_ecliptic 
-        inclination_to_ecliptic_matrix = rotationMatrix_single(2,inclination_to_ecliptic)
+    #     matrixes = np.zeros([goal_angle.shape[0],3,3])
+    #     if axis == 0:
+    #         matrixes[:,1,1] = cosA
+    #         matrixes[:,1,2] = sinA
+    #         matrixes[:,2,1] = -sinA
+    #         matrixes[:,2,2] = cosA
+    #         matrixes[:,0,0] = 1
+    #     if axis == 1:
+
+    #         matrixes[:,0,0] = cosA
+    #         matrixes[:,0,2] = -sinA
+    #         matrixes[:,2,0] = sinA
+    #         matrixes[:,2,2] = cosA
+    #         matrixes[:,1,1] = 1
+    #         if axis == 1:
+    #     ([
+    #         [cosA, 0, -sinA],
+    #         [0, 1, 0],
+    #         [sinA, 0, cosA]])
+    # if axis == 2: 
+    #     return np.array([
+    #         [cosA, sinA, 0],
+    #         [-sinA, cosA, 0],
+    #         [0, 0, 1]])
+
+        #adjustment for Nodal precession
+        day = self.angle_to_day(goal_angle)
+        procession_angle = day / 6793 * 2 * np.pi
+
+        inclination_to_ecliptic = self.inclination_to_ecliptic + procession_angle
+        # inclination_to_ecliptic_matrix = rotationMatrix(2,inclination_to_ecliptic)
         
-        matrix = np.dot(inclination_to_ecliptic_matrix,inclination_matrix)
-        p =  np.einsum('jk,ik->ij', matrix, p)
+        # matrix = np.matmul(inclination_matrix,inclination_to_ecliptic_matrix)
+
+        axis = np.stack([np.cos(inclination_to_ecliptic), 
+                         np.sin(inclination_to_ecliptic),goal_angle*0],axis=1)
+        #print(axis)
+        a = np.deg2rad(self.inclination)
+        #p = p*np.cos(a) + np.cross(axis,p,axis=1)*np.sin(a) + axis*np.dot(axis,p)*(1-np.cos(a))
+        p1 =  p*np.cos(a)
+        p2 = np.cross(axis,p,axis=1)*np.sin(a) 
+        p3 = axis*(np.einsum('ij,ij->i',axis,p)*(1-np.cos(a)))[:,np.newaxis]
+
+        p = p1+p2+p3
+        #print(p)
+        # print('matrix dims',matrix.shape)
+        # p =  np.einsum('ijk,ik->ij', matrix, p)
         return p
 
 
@@ -183,10 +230,10 @@ EarthOrbit = Orbit(f1,f2,a,b,c,e,period,obliquity,lon_adjustment,day_to_angle_ad
 #angles = np.arange(100)*np.pi/100*2
 
 
-n = 365*4
+n = 365*12
 points = np.zeros([1,3],dtype=np.float64)
 days = np.arange(n)/(365.25-12.37)*365.25
-dayse = np.array([254.91667])
+dayse = np.array([255 + 0.70898])
 earthangles = EarthOrbit.day_to_angle(dayse)
 earthloc = EarthOrbit.goal_angle_to_orbital_pos(earthangles)
 toplot = EarthOrbit.get_transformed_point_at_days(points,dayse)
@@ -224,8 +271,8 @@ day_to_angle_adjustment = 2.638937829015426
 
 inclination_to_ecliptic = 4.335397861953914
 
-MoonOrbit = Orbit(f1,f2,a,b,c,e,period,obliquity,
-                  0,day_to_angle_adjustment=day_to_angle_adjustment,
+MoonOrbit = Orbit(f1,f2,a,b,c,e,period,obliquity,inclination=inclination,
+                  day_to_angle_adjustment=day_to_angle_adjustment,
                   inclination_to_ecliptic=inclination_to_ecliptic)
 
 # moonangle = MoonOrbit.day_to_angle(days)
@@ -238,7 +285,7 @@ ax = fig.add_subplot(projection='3d')
 ax.scatter(toplot[:,0],toplot[:,1],toplot[:,2])
 ax.axes.set_xlim3d(left=-1, right=1) 
 ax.axes.set_ylim3d(bottom=-1, top=1) 
-ax.axes.set_zlim3d(bottom=-1, top=1) 
+ax.axes.set_zlim3d(bottom=-1, top=1 ) 
 # num_items = toplot2.shape[0]
 # colors = np.arange(num_items) // 29
 # ax.scatter(toplot2[:,0],toplot2[:,1],toplot2[:,2],c=colors,cmap='hsv')
@@ -268,7 +315,9 @@ def plotmoon3(days):
 #d1 = np.arange(13)*27.321661
 #plotmoon3(days)
 today = np.array([254.91667])
-plotmoon3(today)
+#plotmoon3(today)
+
+plotmoon3(days)
 
 plt.show()
 print(get_moon_loc(np.array([97])),get_sun_loc(np.array([97])))
@@ -279,24 +328,26 @@ print(get_moon_loc(np.array([97])),get_sun_loc(np.array([97])))
 best = [1000000,10000000]
 closest = 10000000000000000
 
-goal = vec(-1.8169,0.137860)
+goal = vec(0.31881,-0.49626)
 
-# for x in range(0,400):
-#     for y in range(0,400):
-#         MoonOrbit = Orbit(f1,f2,a,b,c,e,period,obliquity,0,
-#                           day_to_angle_adjustment = x/100*np.pi*2,inclination_to_ecliptic = y/100*np.pi*2)
+for x in range(0,200):
+    for y in range(-100,100):
+        MoonOrbit=0
+        MoonOrbit = Orbit(f1,f2,a,b,c,e,period,obliquity,inclination=inclination,
+        day_to_angle_adjustment = x/100*np.pi*2,inclination_to_ecliptic = y/100*np.pi*2)
+        #print(MoonOrbit.inclination_to_ecliptic)
+        A = x/100*np.pi*2
+        B = y/100*np.pi*2
+        sl = get_moon_loc(dayse)
+        #print(sl)
+        c = MoonOrbit.get_coords(sl)
+        dist = np.linalg.norm(c-goal)
+        if dist <= closest:
+            print("best")
+            closest = dist + 0
+            print(c,A,B,dist)
 
-#         A = x/100*np.pi*2
-#         B = y/100*np.pi*2
-#         sl = get_moon_loc(dayse)
-#         c = MoonOrbit.get_coords(sl)
-#         dist = np.linalg.norm(c-goal)
-#         if dist < closest:
-#             print("best")
-#             closest = dist + 0
-#             print(c,A,B)
-
-#         #print(c)
+        #print(c)
 
         
         
